5876d954d1b53d20393211984b4b13c8f715822a
Krisa Duwal <101305059+krisaduwal@users.noreply.github.com>
2022-08-09 22:31:41 +0545

    hello world

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head> 
+    <title>first php</title>
+</head>
+    <body>
+        <?php
+        $name = "Krisha Duwal";
+        echo "HELLO WORLD! I am $name";
+        ?>
+    </body>
+</html>
+
